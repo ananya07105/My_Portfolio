1ce91e2ebe434a0b5b2a8caa0f6f92fe864adc3b
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2025-07-30 17:38:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Portfolio from './components/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
